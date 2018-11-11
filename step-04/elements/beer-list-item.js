ef6dd0853284ee5557af42d94b2a5ee575f12cfd
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2018-11-11 23:04:21 +0000

    Initial step-04

---
+++ step-04/elements/beer-list-item.js
@@ -0,0 +1,49 @@
+// Import PolymerElement class and html helper definition
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
+
+// Define the element's class element
+export class BeerListItem extends PolymerElement {
+
+  static get template() {
+    return html`
+      <style>
+        .beer {
+          margin: 10px;
+          padding: 10px;
+          border: solid 1px black;
+          border-radius: 10px;
+          min-height: 50px;
+        }
+      </style>
+      <div class="beer">
+        <h2>[[name]]</h2>
+        <p>[[description]]</p>
+      </div>
+    `;
+  }
+
+  static get properties() {
+    return {
+      name: {
+        type: String,
+      },
+      description: {
+        type: String,
+      }
+    }
+  }
+
+  // Element class can define custom element reactions
+  connectedCallback() {
+    super.connectedCallback();
+    console.log('my-element created!');
+  }
+
+  ready() {
+    super.ready();
+    console.log('my-element is ready!');
+  }
+}
+
+// Associate the new class with an element name
+customElements.define('beer-list-item', BeerListItem);
